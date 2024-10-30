ba1c593f4a228f57506adfa696fa54380bb7e782
bobbycxy <bobbycxy1994@gmail.com>
2024-10-30 18:57:28 +0800

    short term bug fix - need to check

--- textarena/core.py
+++ textarena/core.py
@@ -71,7 +71,7 @@ class State:
                 to_id=player_id,
                 message=player_prompt_function(
                     player_id=player_id,
-                    game_state=self.game_state
+                    # game_state=self.game_state ## TODO: is this suppose to be here?
                 ),
                 for_logging=False
             )
