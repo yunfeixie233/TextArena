221b588a0ce62b80d04754fc8e7790aeb001dc71
guertlerlo <lg11@outlook.com>
2025-02-17 13:26:17 +0800

    added to registry

--- textarena/envs/__init__.py
+++ textarena/envs/__init__.py
@@ -1,2 +1,3 @@
 import textarena.envs.single_player
 import textarena.envs.two_player
+import textarena.envs.multi_player
---
+++ textarena/envs/multi_player/__init__.py
@@ -0,0 +1,40 @@
+""" Register multi-player environments """
+
+from textarena.envs.registration import register
+
+
+# Register Individual Games
+register(
+    id="CharacterConclave-multiplayer-v0",
+    entry_point="textarena.envs.multi_player.CharacterConclave.env:CharacterConclaveEnv",
+    num_players=5,
+    character_budget=1_000,
+)
+
+register(
+    id="CharacterConclave-multiplayer-v0-long",
+    entry_point="textarena.envs.multi_player.CharacterConclave.env:CharacterConclaveEnv",
+    num_players=5,
+    character_budget=5_000,
+)
+
+register(
+    id="CharacterConclave-multiplayer-v0-extreme",
+    entry_point="textarena.envs.multi_player.CharacterConclave.env:CharacterConclaveEnv",
+    num_players=5,
+    character_budget=10_000,
+)
+
+register(
+    id="LiarsDice-multiplayer-v0",
+    entry_point="textarena.envs.multi_player.LiarsDice.env:LiarsDiceEnv",
+    num_players=3,
+    num_dice=5
+)
+
+register(
+    id="LiarsDice-multiplayer-v0-large",
+    entry_point="textarena.envs.multi_player.LiarsDice.env:LiarsDiceEnv",
+    num_players=5,
+    num_dice=12,
+)
